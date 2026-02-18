98379d69294f4e9b09e2b896a2e737c84b5cd0eb
AeshaHangRai <evilaesha@gmail.com>
2026-02-18 14:31:34 +0545

    commit

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
